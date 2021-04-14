bd4babd629bab6cd6780899c28720ea1ecd77faa
Yaroslav <snai_per26@mail.ru>
2021-04-15 01:27:19 +0300

    Исправлены баги. Полностью закончены скрипты движения и анимации

--- New Unity Project (1)/Assets/Scripts/CheckingGround.cs
+++ New Unity Project (1)/Assets/Scripts/CheckingGround.cs
@@ -6,14 +6,15 @@ public class CheckingGround : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnTriggerExit2D(Collider2D Trigger){
-        if(Trigger.gameObject.tag!="Player")
+        if(Trigger.gameObject.tag!="Player"&&Trigger.gameObject.tag!="MainCamera")
         {
           gameObject.GetComponentInParent<Penguin>().Ground = false;
+          
         }
     }
     private void OnTriggerStay2D(Collider2D Trigger)
     {
-        if(Trigger.gameObject.tag!="Player")
+        if(Trigger.gameObject.tag!="Player"&&Trigger.gameObject.tag!="MainCamera")
         {
           gameObject.GetComponentInParent<Penguin>().Ground = true;
         }
--- New Unity Project (1)/Assets/Scripts/Penguin.cs
+++ New Unity Project (1)/Assets/Scripts/Penguin.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class Penguin : MonoBehaviour 
 {
     private Rigidbody2D rb;
-    private Vector2 moveVelocity;
     public float  speed;
     public float jumpHeight;
     public Animator animator;
-    
     public bool Ground;
-    private Vector2 change;
+    private bool facingRight;
    
-    // Start is called before the first frame update
     
     void Start()
     {
@@ -22,116 +19,59 @@ public class Penguin : MonoBehaviour
     }
     void Update()
     {
-
-        
         animator.SetFloat ("vSpeed", rb.velocity.y);
- //       animator.SetBool("Up", false );
- //       animator.SetBool("Down", false);
-//        animator.SetBool("UpAD", false );
-  //      animator.SetBool("DownAD", false );
         animator.SetBool("LeftRight", false );
         
-        if(Ground)
+        
+       if(Ground)
         {
             animator.SetBool("Ground", true);
-        
+            animator.SetBool("Jump 1", false ); // проверка находится ли персонаж в воздухе после прыжка
         }
         else animator.SetBool("Ground", false);
         
         
-        //change.y = Input.GetAxisRaw("Vertical");
-        if(Input.GetKeyDown(KeyCode.Space)&&Ground&&!Input.GetKey(KeyCode.D)&&!Input.GetKey(KeyCode.A))//--------------------Прыжок/рывок
+        if(Input.GetKeyDown(KeyCode.Space)&&Ground&&!Input.GetKey(KeyCode.D)&&!Input.GetKey(KeyCode.A))//--------Прыжок/рывок при АФК
                 {                                      
                     rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
-                    //animator.SetFloat ("vSpeed", rb.velocity.y);
+                    animator.SetBool("Jump 1", true );
                 }
         
         
-        if(Input.GetKey(KeyCode.D))//--------------------Прыжок/рывок
-            {                                       
-                rb.AddForce(transform.right * speed, ForceMode2D.Impulse);
-                //transform.rotation = Quaternion.Euler(0, 180 ,0);
-                animator.SetBool("LeftRight", true); 
+        if(Input.GetKey(KeyCode.D))//----------------------------------------------------------------------------Право
+        {                                       
+            if(!facingRight)Flip();
+            rb.AddForce(transform.right * speed, ForceMode2D.Impulse);   
+            //transform.rotation = Quaternion.Euler(0, 180 ,0);
+            animator.SetBool("LeftRight", true); 
                 
-                if(Input.GetKeyDown(KeyCode.Space)&&Ground)//--------------------Прыжок/рывок
-                {                                      
-                    rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
-                    animator.SetTrigger("Jump");
-                    //Falling();
-                }
-            }else
-        if(Input.GetKey(KeyCode.A))//--------------------Прыжок/рывок
-            {                    
+            if(Input.GetKeyDown(KeyCode.Space)&&Ground)//--------------------------------------------------------Прыжок/рывок
+            {                                      
+                rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+                animator.SetTrigger("Jump");
+                animator.SetBool("Jump 1", true );
+            }
+        }else
+            if(Input.GetKey(KeyCode.A))//------------------------------------------------------------------------Лево
+            {   
+                if(facingRight)Flip();                   
                 rb.AddForce(-transform.right * speed, ForceMode2D.Impulse);
                 //transform.rotation = Quaternion.Euler(0, 0 ,0); 
                 animator.SetBool("LeftRight", true);
-                if(Input.GetKeyDown(KeyCode.Space)&&Ground)//--------------------Прыжок/рывок
+                if(Input.GetKeyDown(KeyCode.Space)&&Ground)//-----------------------------------------------------Прыжок/рывок
                 {                                      
                     rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
                     animator.SetTrigger("Jump");
-                   // Falling();
+                    animator.SetBool("Jump 1", true );
                 }
-            }
-            
-        /*if(change.x>0.01f)//------------------------------------------Вперед
-        {                                           
-            animator.SetBool("Up", true );
-            if(change.x<-0.01f||change.x>0.01f) //--------------------Запуск анимации по диагонали вниз-вбок
-            {                                        
-                animator.SetBool("UpAD", true );
-            }
-            if((Input.GetKeyDown(KeyCode.Space))&&(Input.GetKey("w")))//--------------------Прыжок/рывок
-            {                                       
-                animator.SetTrigger("Jump" );
-            }
-        } else 
-        if(change.x<-0.01f) //----------------------------------------Назад
-        {                                           
-            animator.SetBool("Down", true );
-            if(change.x<-0.01f||change.x>0.01f)//---------------------Запуск анимации по диагонали вниз-вбок
-            {                                       
-                animator.SetBool("DownAD", true );
-            }
-            if((Input.GetKeyDown(KeyCode.Space))&&(Input.GetKey("s")))//--------------------Прыжок/рывок
-            {                                       
-                animator.SetTrigger("Jump" );
-            }
-        }/*
-        if(change.x<-0.01f)//-----------------------------------------Влево
-        {                                           
-            transform.rotation = Quaternion.Euler(0, 0 ,0);
-            animator.SetBool("LeftRight",true);
+        }
+    }
 
-            if(Input.GetKeyDown(KeyCode.Space))//---------------------Прыжок/рывок
-            {                                       
-                animator.SetTrigger("Jump" );
-            }
-            if(change.y>0.01f)//--------------------------------------Запуск анимации по диагонали вверх-вбок 
-            {                                       
-                animator.SetBool("UpAD", true );
-            }else 
-            if(change.y<-0.01f)//-------------------------------------Запуск анимации по диагонали вниз-вбок
-            {                                        
-                    animator.SetBool("DownAD", true );
-            }
-        } else if(change.x>0.01f)//----------------------------------Вправо
-        {                                           
-            transform.rotation = Quaternion.Euler(0, 180 ,0);
-            animator.SetBool("LeftRight",true);
-            if(change.y>0.01f)//-------------------------------------Запуск анимации по диагонали вверх-вбок 
-            {                                       
-                animator.SetBool("UpAD", true );//-------------------Запуск анимации по диагонали вниз-вбок
-            }else 
-            if(change.y<-0.01f)
-            {                                        
-                animator.SetBool("DownAD", true );
-            }
-            if(Input.GetKeyDown(KeyCode.Space))//--------------------Прыжок/рывок
-            {                                       
-                animator.SetTrigger("Jump" );
-            }
-        }  */
-    } 
-    
-    
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
 }
