49ebbb2e7aed203acbbca204c90d2089453284f4
Yaroslav <snai_per26@mail.ru>
2021-05-02 01:33:26 +0300

    Исправлен баг с бегом в стену

--- New Unity Project (1)/Assets/Scripts/Penguin.cs
+++ New Unity Project (1)/Assets/Scripts/Penguin.cs
@@ -5,35 +5,33 @@ using UnityEngine;
 public class Penguin : MonoBehaviour 
 {
     private Rigidbody2D rb;
+    private Rigidbody rbb;
+    private Collider2D coll;
     public float  speed;
-    public float speedMobile;
+    public float normalSpeed;
     public float jumpHeight;
     public float moveInput;
     public Animator animator;
     public bool Ground;
     private bool facingRight;
-    public float normalSpeed;
+
+
 
     public Joystick joystick; 
-   public float n;
     
     void Start()
     {
         //speed=0f;
         rb=GetComponent<Rigidbody2D>();
+        rbb = GetComponent<Rigidbody>();
+        coll = gameObject.GetComponent<Collider2D>();
     }
     
-    void Update()
+    void FixedUpdate()
     {
-        var MaxSpeed = rb.velocity.magnitude;
-        
-        //--------------------------------Кнопки для телефона----------------------------------------
-        //float h = Input.GetAxisRaw("Horizontal");
-        moveInput = joystick.Horizontal;
-        //rb.AddForce(Vector2.right * h * speed);
-        //rb.velocity = new Vector2(moveInput * speedMobile, rb.velocity.y);
-
-        //--------------------------------------Конец------------------------------------------------
+        moveInput = Input.GetAxisRaw("Horizontal");
+        //-------------------------------------------
+        //moveInput = joystick.Horizontal;
 
         animator.SetFloat ("vSpeed", rb.velocity.y);
         animator.SetBool("LeftRight", false );
@@ -47,25 +45,27 @@ public class Penguin : MonoBehaviour
         
         
         if(Input.GetKeyDown(KeyCode.Space)&&Ground&&!Input.GetKey(KeyCode.D)&&!Input.GetKey(KeyCode.A))//--------Прыжок/рывок при АФК
-                {                                      
-                    //rb.velocity = new Vector2(rb.velocity.y*speed, rb.velocity.y);
-                    rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
-                    animator.SetBool("Jump 1", true );
-                }
+        {                                      
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+            animator.SetBool("Jump 1", true );
+        }
         
         
         if(Input.GetKey(KeyCode.D)||moveInput>0.01f)//----------------------------------------------------------------------------Право
         {                                       
-            if(!facingRight)Flip();
-            //moveInput = Input.GetAxisRaw("Horizontal");
-            //rb.velocity = new Vector2(moveInput*speedMobile, rb.velocity.y);
-            //if(MaxSpeed<10||Jump)
-            rb.AddForce(transform.right * speed, ForceMode2D.Force);
-            MaxSpeed = rb.velocity.magnitude;   
-            animator.SetBool("LeftRight", true); 
-                
+            if(!facingRight)
+                Flip();
+
+            if(rb.velocity.magnitude<normalSpeed)
+                rb.AddForce(new Vector2(speed,0), ForceMode2D.Force);
+            else
+                rb.velocity = new Vector2(moveInput*normalSpeed, rb.velocity.y);
+
+            //Debug.Log("Speed: "+rb.velocity.magnitude);
+
+            animator.SetBool("LeftRight", true);     
             if(Input.GetKeyDown(KeyCode.Space)&&Ground)//--------------------------------------------------------Прыжок/рывок
-            {                                      
+            {                                 
                 rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
                 animator.SetTrigger("Jump");
                 animator.SetBool("Jump 1", true );
@@ -74,11 +74,16 @@ public class Penguin : MonoBehaviour
         
         if(Input.GetKey(KeyCode.A)||moveInput<-0.01f)//------------------------------------------------------------------------Лево
         {   
-            Debug.Log("Speed: "+MaxSpeed);
             if(facingRight)
-                Flip();                  
-            rb.AddForce(-transform.right * speed, ForceMode2D.Impulse);
-            MaxSpeed = rb.velocity.magnitude;
+                Flip();
+
+            if(rb.velocity.magnitude<normalSpeed)
+                rb.AddForce(new Vector2(-speed,0), ForceMode2D.Force);
+            else
+                rb.velocity = new Vector2(moveInput*normalSpeed, rb.velocity.y);
+
+            //Debug.Log("Speed: "+rb.velocity.magnitude);
+
             animator.SetBool("LeftRight", true);
             if(Input.GetKeyDown(KeyCode.Space)&&Ground)//-----------------------------------------------------Прыжок/рывок
             {                                      
@@ -93,8 +98,6 @@ public class Penguin : MonoBehaviour
     {
         if(Ground)rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
     }
-
-
 
     void Flip()
     {
